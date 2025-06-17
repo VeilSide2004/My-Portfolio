13846872c96b565143eb738e556dc32e3abbd83b
VeilSide2004 <mondalaranya925@gmail.com>
2025-06-17 20:33:40 +0530

    first commit

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+
+
+const StarBackground = () => {
+
+    const [stars, setStars] = useState([])
+
+    useEffect(() => {
+        generateStars()
+    }, [])
+
+    const generateStars = () => {
+        const numberOfStarts = Math.floor(window.innerWidth * window.innerHeight / 10000)
+
+        const newStars = []
+
+        for (let i = 0; i < numberOfStarts; i++) {
+            newStars.push({
+                id: i,
+                size: Math.random() * 3 + 1,
+                x: Math.random() * 100,
+                y: Math.random() * 100,
+                opacity: Math.random() * 0.5 + 0.5,
+                animationDuration: Math.random() * 4 + 2,
+
+            })
+        }
+
+        setStars(newStars)
+    }
+    return (
+        <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
+            {stars.map((star) => (
+                <div key={star.id} className="star animate-pulse-subtle" style={{
+                    height: star.size + "px",
+                    width: star.size + "px",
+                    left: star.x + "%",
+                    top: star.y + "%",
+                    opacity: star.opacity,
+                    animationDuration: star.animationDuration + "s",
+                }} />
+            ))}
+
+        </div>
+    );
+};
+
+export default StarBackground;
