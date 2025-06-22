0b2e3f8f11dbd2d26549b5c335990985e3606e11
Aranya Mondal <118832722+VeilSide2004@users.noreply.github.com>
2025-06-22 21:10:58 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import About from "../components/About";
 import Certificates from "../components/Certificates";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Project from "../components/Project";
@@ -13,23 +14,27 @@ const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             {/* Theme toggle complete */}
-            <ThemeToggle/>
+            <ThemeToggle />
 
             {/* the star and meteor background */}
-            <StarBackground/>
+            <StarBackground />
 
             {/* this is the navbar */}
-            <Navbar/>
+            <Navbar />
 
             {/* the main content */}
             <main>
-                <Hero/>
-                <About/>
-                <Skills/>
-                <Project/>
-                <Certificates/>
-                <Contact/>
+                <Hero />
+                <About />
+                <Skills />
+                <Project />
+                <Certificates />
+                <Contact />
             </main>
+
+            {/* Footer */}
+            <Footer />
+
         </div>
     );
 };
